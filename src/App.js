f39f90abf3eba3f4bd802de90b80ae30be9a171b
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2022-05-20 17:43:03 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import {Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Box} from '@mui/material'
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+    <CssBaseline/>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1, marginLeft: 5 }}>
+            News
+          </Typography>
+          <Typography>
+            HOME
+          </Typography>
+          <Typography sx={{marginLeft: 5, marginRight:10}}>
+            ADD CAR
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
 
