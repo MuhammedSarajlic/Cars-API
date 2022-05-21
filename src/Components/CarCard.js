f5b80e5cd62c18981badf8b401f6e5c524dfeeb1
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2022-05-21 19:43:11 +0200

    Card update

--- src/Components/CarCard.js
+++ src/Components/CarCard.js
@@ -1,11 +1,9 @@
-import {Typography, Button, Card, CardActions, CardContent, CardMedia, Grid} from '@mui/material'
+import {Typography, Button, Card, CardActions, CardContent, CardMedia, Grid, TextField, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions} from '@mui/material'
+import { useState } from 'react'
 
-  const edit = (e) => {
-    e.preventDefault()
-    console.log("Edited")
-  }
 
 const CarCard = ({ car, setCars, cars }) => {
+    const [open, setOpen] = useState(false)
 
     const deleteHandler = () => {
         fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars/${car.id}`, {
@@ -13,9 +11,13 @@ const CarCard = ({ car, setCars, cars }) => {
         })
         .then(res => {
             console.log(res)
-            console.log("Succesfuly deleted", car.name)
+            console.log("Successfully deleted", car.name)
         })
         setCars(cars.filter((el) => el.id !== car.id))
+    }
+
+    const editHandler = () => {
+        console.log("Edited")
     }
 
   return (
@@ -33,18 +35,44 @@ const CarCard = ({ car, setCars, cars }) => {
             />
             <CardContent sx={{flexGrow: 1}}>
                 <Typography gutterBottom variant='h5'>
-                {car.name}
+                {`${car.manufacturer} ${car.name}`}
+                </Typography>
+                <Typography variant='subtitle2'>
+                {`Year: ${car.year}`}
                 </Typography>
                 <Typography>
-                {car.manufacturer}
-                </Typography>
+                {`Price: ${car.price} KM`}
+                </Typography> 
             </CardContent>
             <CardActions>
-                <Button onClick={edit} size="small" variant="contained" color="primary">Edit</Button>
+                <Button onClick={() => setOpen(true)} size="small" variant="contained" color="primary">Edit</Button>
                 <Button onClick={deleteHandler} size="small" variant="outlined" color="error">Delete</Button>
             </CardActions>
             </Card>
         </Grid>
+        <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+            aria-labelledby='dialog-title'
+            aria-describedby='dialog-description'
+        >
+            <DialogTitle id="dialog-title">
+                Edit car
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="dialog-description" sx={{display: 'flex', flexDirection: 'column'}}>
+                    <TextField id="outlined-basic" label="Name" variant="outlined" sx={{ width: 400, marginTop: '20px' }}/>
+                    <TextField id="outlined-basic" label="Manufacturer" variant="outlined" sx={{ width: 400, marginTop: '20px' }}/>
+                    <TextField id="outlined-basic" label="Image Url" variant="outlined" sx={{ width: 400, marginTop: '20px' }}/>
+                    <TextField id="outlined-basic" label="Price" variant="outlined" sx={{ width: 400, marginTop: '20px' }}/>
+                    <TextField id="outlined-basic" label="Year" variant="outlined" sx={{ width: 400, marginTop: '20px' }}/>
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions sx={{padding: '0 25px 20px 0'}}>
+                <Button onClick={() => setOpen(false)}>Cancel</Button>
+                <Button onClick={() => setOpen(false)} variant='contained'>Submit</Button>
+            </DialogActions>
+        </Dialog>
     </>
   )
 }
