89c7791933aa5f16ad7ca4e5748bf0c75576f82b
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2022-05-22 13:16:48 +0200

    POST method and add in array

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,7 +7,36 @@ const Main = ({ setCars, cars }) => {
     const [open, setOpen] = useState(false)
     const [carData, setCarData] = useState({})
 
-    
+    const addHandler = (e) => {
+        e.preventDefault()
+        fetch(URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                id: Math.round(Math.random() * 1000),
+                name: carData.name,
+                manufacturer: carData.manufacturer,
+                imageUrl: carData.imageUrl,
+                price: carData.price,
+                year: carData.year
+            })
+        })
+        .then(res => {
+            if(res.ok) console.log("Successfully updated", res)
+        })
+        .catch(err => console.log(err))
+        setCars([
+            ...cars, {
+                id: Math.round(Math.random() * 1000),
+                name: carData.name,
+                manufacturer: carData.manufacturer,
+                imageUrl: carData.imageUrl,
+                price: carData.price,
+                year: carData.year
+            }
+        ])
+        setOpen(false)
+    }
 
   return (
     <>
@@ -40,7 +69,7 @@ const Main = ({ setCars, cars }) => {
                 </DialogContent>
                 <DialogActions sx={{padding: '0 25px 20px 0'}}>
                     <Button onClick={() => setOpen(false)}>Cancel</Button>
-                    <Button /*onClick={addHandler}*/ variant='contained'>Submit</Button>
+                    <Button onClick={addHandler} variant='contained'>Submit</Button>
                 </DialogActions>
             </Dialog>
         </div>
